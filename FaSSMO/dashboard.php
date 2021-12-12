a1fe60b44cfae134a3a69ade7c4873c17c490338
reykmund <95955508+reykmund@users.noreply.github.com>
2021-12-12 19:50:49 +0800

    Update dashboard.php

--- FaSSMO/dashboard.php
+++ FaSSMO/dashboard.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="OnlineAttendance/img/fassmo_black.png" type="image/png">
 	<link rel="stylesheet" href="OnlineAttendance/css/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <style>
